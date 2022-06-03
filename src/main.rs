0d0044e97f984b21e4aae4e48c296a2d38373ac9
Alper Buluç <alper.buluc@gmail.com>
2022-06-03 20:40:42 +0300

    remove: main

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use elrond_growth_rust::{market_value, EGResult};
-
-#[tokio::main]
-async fn main() -> EGResult<()> {
-    let price_data = market_value::price().await?;
-    println!("{:#?}", price_data.last().unwrap().value);
-    Ok(())
-}
